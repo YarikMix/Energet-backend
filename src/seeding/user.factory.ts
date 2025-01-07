1d05e0a23a152f64212c022b83f7aac6440a9606
YarikMix <43493788+YarikMix@users.noreply.github.com>
2025-01-08 00:53:49 +0300

    User factory password fix

--- src/seeding/user.factory.ts
+++ src/seeding/user.factory.ts
@@ -2,13 +2,15 @@ import { setSeederFactory } from 'typeorm-extension';
 import { User } from '@entities/user/models/user.entity';
 import { E_UserType } from '@entities/user/models/types';
 import { faker } from '@faker-js/faker';
+import * as bcrypt from 'bcrypt';
 
-export const UserFactory = setSeederFactory(User, () => {
+export const UserFactory = setSeederFactory(User, async () => {
   const user = new User();
   user.name = faker.person.firstName() + ' ' + faker.person.lastName();
   user.email = faker.internet.email();
   user.phone = faker.phone.number();
-  user.password = faker.internet.password();
+  user.password = await bcrypt.hash('1234', 10);
+  // user.password = await bcrypt.hash(faker.internet.password(), 10);
   user.role = E_UserType.Buyer;
   return user;
 });
